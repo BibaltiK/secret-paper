8491859f353144418b960d686a870d8b0e714ce3
BibaltiK <github@exdrals.de>
2021-11-16 20:08:05 +0100

    memory limit

--- public/index.php
+++ public/index.php
@@ -3,5 +3,6 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 date_default_timezone_set('Europe/Berlin');
+ini_set ( 'memory_limit','256M' );
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'index.php';
